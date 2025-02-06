8cbf02fb3c097dedcc3e3a887b3f35d021c5051e
Eunum56 <msiaffu123@gmail.com>
2025-02-06 14:58:13 +0530

    Add DeployMyToken script to deploy

---
+++ script/DeployMyToken.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract DeployMyToken is Script {
+    uint256 private constant INITIAL_SUPPLY = 1000 ether;
+    function run() external returns (MyToken) {
+        vm.startBroadcast();
+        MyToken myToken = new MyToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return myToken;
+    }
+}
